4ef244864a3cd7645c60a02b95e36bf1fcd8de5a
artimmy <arthurtimmy@hotmail.com>
2022-03-10 17:43:13 -0300

    adding Login(), and ThemeData()

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/home.dart';
+import 'package:whatsapp_clone/login.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "WhatsApp",
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: const Color(0xff075E54),
+      // ignore: deprecated_member_use
+      accentColor: const Color(0xff25D366),
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
